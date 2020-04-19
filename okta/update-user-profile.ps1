ab08a01e41debeb028a39146e8e085639d291a1f
Shaneil <53551518+shnlnryn@users.noreply.github.com>
2020-04-19 19:43:29 +1000

    Create update-user-profile.ps1

---
+++ okta/update-user-profile.ps1
@@ -0,0 +1,22 @@
+$header = @{
+Authorization = "SSWS _API KEY HERE_"
+Accept = "application/json"
+}
+
+$list = -split @"
+user1@mydomain.com
+user2@mydomain.com
+user3@mydomain.com
+
+
+"@
+
+$list | For-Each-Object {
+
+  Write-Verbose -Verbose $_
+ 
+  try {
+  
+   $login = $_
+   $uri = "https://company-admin.okta.com/api/v1/users/$login"
+   
