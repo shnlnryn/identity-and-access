8f935ee58bacd75ddfbc3140f4225579f56de84f
Shaneil <53551518+shnlnryn@users.noreply.github.com>
2021-01-27 16:07:28 +1100

    Update get-groupmembers-userprofiles.ps1

--- okta/get-groupmembers-userprofiles.ps1
+++ okta/get-groupmembers-userprofiles.ps1
@@ -1,3 +1,6 @@
+#This script gets members from a specified group and their user profile and MFA status.
+
+
 
 $header = @{
 Authorization = "SSWS ####API KEY###"
@@ -26,21 +29,42 @@ $users| ForEach-Object {
 
     $uProf = Invoke-RestMethod -Uri $u -Headers $header -Method GET -SslProtocol:Tls12
 
+    $uid = $uProf.id
     
+# Get Factor status
+    $factors="https://###TENANT###-admin.okta.com/api/v1/users/$uid/factors"
+    $uf=Invoke-RestMethod -Uri $factors -Headers $header -Method GET -SslProtocol:Tls12
+
+            $sms = $uf.where({$_.factorType -eq "sms"})
+            $oktaverify = $uf.where({$_.factorType -eq "push"})
+            $WebAuthn = $uf.where({$_.factorType -eq "webauthn"})
+            $FIDO = $uf.where({$_.factorType -eq "token:hardware"})
+            $OKTAOTP = $uf.where({$_.factorType -eq "token:software:totp"})
+
+            
 
 
+    #Create report and export
     $report = [pscustomobject]@{
         id = $uProf.id
         firstName = $uProf.profile.firstName
         login = $uProf.profile.login
+        department=$uProf.profile.department
+        manager=$uProf.profile.manager
         created = $uProf.created
-        lastlogin = $uProf.lastlogin
         passwordChanged = $uprof.passwordChanged
+        lastlogin = $uProf.lastlogin
         lastUpdated = $uProf.lastUpdated
         status = $uProf.status
+        sms = $sms.status
+        oktaverify= $oktaverify.status
+        FIDO= $FIDO.status
+        oktaotp=$OKTAOTP.status
+
+
         }
 
-        $report| Export-Csv -Append -path /users/shnl/documents/okta_Vol_users_report_$((get-date -Format "yyyyMMdd")).csv -NoTypeInformation -Force
+        $report| Export-Csv -Append -path /users/shnlnryn/documents/okta_Vol_users_report_$((get-date -Format "yyyyMMdd")).csv -NoTypeInformation -Force
 
 }
 
