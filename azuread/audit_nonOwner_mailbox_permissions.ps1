e8d0c80d1470071025c67aba6d4e9145cbfd4a29
Shaneil <53551518+shnlnryn@users.noreply.github.com>
2022-05-25 09:48:20 +1000

    Create audit_nonOwner_mailbox_permissions.ps1

---
+++ azuread/audit_nonOwner_mailbox_permissions.ps1
@@ -0,0 +1,24 @@
+# Non Owner mailbox permission audit
+
+$mb= Get-Mailbox -resultsize unlimited | Where {($_.RecipientTypeDetails -ne "RoomMailbox")}
+
+$mb | ForEach-Object {
+
+    $mailbox = $_.userprincipalname
+    $mailbox
+
+
+
+$perm = Get-MailboxPermission -Identity $mailbox | Select Identity, User, AccessRights| Where {($_.user -ne "NT AUTHORITY\SELF")}
+
+
+$report = [pscustomobject]@{
+    MailboxUPN = $mailbox
+    Identity = $perm.identity
+    Delegate = $perm.User
+    AccessRights = $perm.AccessRights
+}
+
+}
+
+$report| Export-Csv -Append -path d:\temp\MailboxPermissions_fromList.csv -NoTypeInformation -Force
